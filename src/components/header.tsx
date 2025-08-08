f18f7b3174e9839b89d02efde58947523dc70328
tawan <thiago_tawan@outlook.com>
2025-08-08 10:03:23 -0300

    feat: adding header and exclude css style

---
+++ src/components/header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <div className="p-4 bg-black">
+      <p>Video Platform</p>
+      <div className="flex items-center gap-4">
+        <button>Log In</button>
+        <button>Sign Up</button>
+      </div>
+    </div>
+  );
+}
